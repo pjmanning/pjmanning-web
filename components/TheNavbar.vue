a620de5c7486d2202ed032333b52a172dce3601b
PJ Manning <phil.manning11@gmail.com>
2021-08-30 20:10:47 +0800

    moving the navbar buttons so contact is last

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -13,10 +13,10 @@
                             <nuxt-link v-else to="/#projects" v-scroll-to="'#projects'" class="inline-flex items-center px-1 pt-1 font-medium text-gray-800 uppercase hover:text-gray-600"> Projects </nuxt-link>
                             <a v-if="$route.path === '/'" href="/#about" v-scroll-to="'#about'" class="inline-flex items-center px-1 pt-1 font-medium text-gray-800 uppercase hover:text-gray-600"> About </a>
                             <nuxt-link v-else to="/#about" v-scroll-to="'#about'" class="inline-flex items-center px-1 pt-1 font-medium text-gray-800 uppercase hover:text-gray-600"> About </nuxt-link>
-                            <a v-if="$route.path === '/'" href="/#contact" v-scroll-to="'#contact'" class="inline-flex items-center px-1 pt-1 font-medium text-gray-800 uppercase hover:text-gray-600"> Contact </a>
-                            <nuxt-link v-else to="/#contact" v-scroll-to="'#contact'" class="inline-flex items-center px-1 pt-1 font-medium text-gray-800 uppercase hover:text-gray-600"> Contact </nuxt-link>
                             <nuxt-link to="/blog" class="inline-flex items-center px-1 pt-1 font-medium text-gray-800 uppercase hover:text-gray-600"> Blog </nuxt-link>
                             <a href="https://www.getrevue.co/profile/pjmanning" target="_blank" class="inline-flex items-center px-1 pt-1 font-medium text-gray-800 uppercase hover:text-gray-600"> Newsletter </a>
+                            <a v-if="$route.path === '/'" href="/#contact" v-scroll-to="'#contact'" class="inline-flex items-center px-1 pt-1 font-medium text-gray-800 uppercase hover:text-gray-600"> Contact </a>
+                            <nuxt-link v-else to="/#contact" v-scroll-to="'#contact'" class="inline-flex items-center px-1 pt-1 font-medium text-gray-800 uppercase hover:text-gray-600"> Contact </nuxt-link>
                         </div>
                     </div>
                     <div class="flex items-center -mr-2 sm:hidden">
@@ -41,10 +41,10 @@
                     <nuxt-link v-else to="/#projects" v-scroll-to="'#projects'" class="block py-2 pl-3 pr-4 text-base font-medium text-gray-500 border-l-4 border-transparent hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700">Projects</nuxt-link>
                     <a v-if="$route.path === '/'" href="/#about" v-scroll-to="'#about'" @click="isMenuOpen = !isMenuOpen" class="block py-2 pl-3 pr-4 text-base font-medium text-gray-500 border-l-4 border-transparent hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700">About</a>
                     <nuxt-link v-else to="/#about" v-scroll-to="'#about'" class="block py-2 pl-3 pr-4 text-base font-medium text-gray-500 border-l-4 border-transparent hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700">About</nuxt-link>
-                    <a v-if="$route.path === '/'" href="/#contact" v-scroll-to="'#contact'" @click="isMenuOpen = !isMenuOpen" class="block py-2 pl-3 pr-4 text-base font-medium text-gray-500 border-l-4 border-transparent hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700">Contact</a>
-                    <nuxt-link v-else to="/#contact" v-scroll-to="'#contact'" class="block py-2 pl-3 pr-4 text-base font-medium text-gray-500 border-l-4 border-transparent hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700">Contact</nuxt-link>
                     <nuxt-link to="/blog" @click.native="isMenuOpen = !isMenuOpen" class="block py-2 pl-3 pr-4 text-base font-medium text-gray-500 border-l-4 border-transparent hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700">Blog</nuxt-link>
                     <a href="https://www.getrevue.co/profile/pjmanning" target="_blank" @click="isMenuOpen = !isMenuOpen" class="block py-2 pl-3 pr-4 text-base font-medium text-gray-500 border-l-4 border-transparent hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700">Newsletter</a>
+                    <a v-if="$route.path === '/'" href="/#contact" v-scroll-to="'#contact'" @click="isMenuOpen = !isMenuOpen" class="block py-2 pl-3 pr-4 text-base font-medium text-gray-500 border-l-4 border-transparent hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700">Contact</a>
+                    <nuxt-link v-else to="/#contact" v-scroll-to="'#contact'" class="block py-2 pl-3 pr-4 text-base font-medium text-gray-500 border-l-4 border-transparent hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700">Contact</nuxt-link>
                 </div>
             </div>
         </nav>

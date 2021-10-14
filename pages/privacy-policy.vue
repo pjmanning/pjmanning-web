aeefee0e659842cb47fbbbe92dae71c1e48f4154
PJ Manning <phil.manning11@gmail.com>
2021-10-15 00:26:41 +0800

    new privacy policy

--- pages/privacy-policy.vue
+++ pages/privacy-policy.vue
@@ -1,48 +1,96 @@
 <template>
-    <div class="max-w-5xl px-4 mx-auto">
-        <h1 class="pb-4 text-5xl text-center">Privacy Policy</h1>
-        <p>Your privacy is important to us.</p>
+    <div class="max-w-5xl px-4 mx-auto my-20 prose">
+        <h1 class="pb-4 text-5xl text-center">Privacy Policy's</h1>
+        <div id="doodDevelopments">
+            <h2>doodDevelopments & PJManning.dev's Policy</h2>
+            <p>Your privacy is important to us.</p>
 
-        <p>The following Privacy Policy governs the online information collection practices of doodDevelopments, LLC (pjmanning.dev) hereby referred to as “we” or “us”. Specifically, it outlines the types of information that we gather about you while you are using the pjmanning.dev website, and the ways in which we use this information.</p>
+            <p>The following Privacy Policy governs the online information collection practices of doodDevelopments, LLC hereby referred to as “we” or “us”. Specifically, it outlines the types of information that we gather about you while you are using the pjmanning.dev website, and the ways in which we use this information.</p>
 
-        <p>It is doodDevelopments, LLC's policy to respect your privacy regarding any information we may collect while operating our website. Accordingly, we have developed this privacy policy in order for you to understand how we collect, use, communicate, disclose and otherwise make use of personal information. We have outlined our privacy policy below.</p>
+            <p>It is doodDevelopments, LLC's policy to respect your privacy regarding any information we may collect while operating our website. Accordingly, we have developed this privacy policy in order for you to understand how we collect, use, communicate, disclose and otherwise make use of personal information. We have outlined our privacy policy below.</p>
 
-        <ul>
-            <li>We will collect personal information by lawful and fair means and, where appropriate, with the knowledge or consent of the individual concerned.</li>
-            <li>Before or at the time of collecting personal information, we will identify the purposes for which information is being collected.</li>
-            <li>We will collect and use personal information solely for fulfilling those purposes specified by us and for other ancillary purposes, unless we obtain the consent of the individual concerned or as required by law.Personal data should be relevant to the purposes for which it is to be used, and, to the extent necessary for those purposes, should be accurate, complete, and up-to-date.</li>
-            <li>
-                We collect and store information that is generated automatically as you navigate online through the website. We also may use a standard feature found in browser software called a “cookie” to enhance your experience with the website. Cookies are small files that your web browser places on your hard drive for record-keeping purposes. By showing how and when visitors use the Site, cookies help us deliver advertisements, identify how many unique users visit us, and track user trends
-                and patterns. They also prevent you from having to re-enter your preferences on certain areas of the Site where you may have entered preference information before.
-            </li>
-            <li>We also use Google Analytics Advertiser Features to optimize our business. Advertiser features include Remarketing with Google Analytics, Google Display Network Impression Reporting, DoubleClick Platform integrations and Google Analytics Demographics and Interest Reporting.</li>
+            <ul>
+                <li>We will collect personal information by lawful and fair means and, where appropriate, with the knowledge or consent of the individual concerned.</li>
+                <li>Before or at the time of collecting personal information, we will identify the purposes for which information is being collected.</li>
+                <li>We will collect and use personal information solely for fulfilling those purposes specified by us and for other ancillary purposes, unless we obtain the consent of the individual concerned or as required by law.Personal data should be relevant to the purposes for which it is to be used, and, to the extent necessary for those purposes, should be accurate, complete, and up-to-date.</li>
+                <li>
+                    We collect and store information that is generated automatically as you navigate online through the website. We also may use a standard feature found in browser software called a “cookie” to enhance your experience with the website. Cookies are small files that your web browser places on your hard drive for record-keeping purposes. By showing how and when visitors use the Site, cookies help us deliver advertisements, identify how many unique users visit us, and track user
+                    trends and patterns. They also prevent you from having to re-enter your preferences on certain areas of the Site where you may have entered preference information before.
+                </li>
+                <li>We also use Google Analytics Advertiser Features to optimize our business. Advertiser features include Remarketing with Google Analytics, Google Display Network Impression Reporting, DoubleClick Platform integrations and Google Analytics Demographics and Interest Reporting.</li>
 
-            <p>
-                By enabling these Google Analytics Display features, we are required to notify our visitors by disclosing the use of these features and that we and third-party vendors use first-party cookies (such as the Google Analytics cookie) or other first-party identifiers, and third-party cookies (such as the DoubleClick cookie) or other third-party identifiers together to gather data about your activities on our Site. Among other uses, this allows us to contact you if you begin to fill
-                out our checkout form but abandon it before completion with an email reminding you to complete your order. The “Remarketing” feature allows us to reach people who previously visited our Site, and match the right audience with the right advertising message.You can opt out of Google’s use of cookies by visiting Google’s ad settings and/or you may opt out of a third-party vendor's use of cookies by visiting the
-                <a href="http://optout.networkadvertising.org/" target="_blank">Network Advertising Initiative opt-out</a>
-                page.
-            </p>
-            <li>As advertisers on Facebook and through our Facebook page, we, (not Facebook) may collect content or information from a Facebook user and such information may be used in the same manner specified in this Privacy Policy. You consent to our collection of such information.We abide by Facebook’s Data Use Restrictions.</li>
+                <p>
+                    By enabling these Google Analytics Display features, we are required to notify our visitors by disclosing the use of these features and that we and third-party vendors use first-party cookies (such as the Google Analytics cookie) or other first-party identifiers, and third-party cookies (such as the DoubleClick cookie) or other third-party identifiers together to gather data about your activities on our Site. Among other uses, this allows us to contact you if you begin to
+                    fill out our checkout form but abandon it before completion with an email reminding you to complete your order. The “Remarketing” feature allows us to reach people who previously visited our Site, and match the right audience with the right advertising message.You can opt out of Google’s use of cookies by visiting Google’s ad settings and/or you may opt out of a third-party vendor's use of cookies by visiting the
+                    <a href="http://optout.networkadvertising.org/" target="_blank">Network Advertising Initiative opt-out</a>
+                    page.
+                </p>
+                <li>As advertisers on Facebook and through our Facebook page, we, (not Facebook) may collect content or information from a Facebook user and such information may be used in the same manner specified in this Privacy Policy. You consent to our collection of such information.We abide by Facebook’s Data Use Restrictions.</li>
 
-            <p>Any ad data collected, received or derived from our Facebook ad (“Facebook advertising data”) is only shared with someone acting on our behalf, such as our service provider. We are responsible for ensuring that our service providers protect any Facebook advertising data or any other information obtained from us, limit our use of all of that information, and keep it confidential and secure.</p>
+                <p>Any ad data collected, received or derived from our Facebook ad (“Facebook advertising data”) is only shared with someone acting on our behalf, such as our service provider. We are responsible for ensuring that our service providers protect any Facebook advertising data or any other information obtained from us, limit our use of all of that information, and keep it confidential and secure.</p>
 
-            <p>We do not use Facebook advertising data for any purpose (including retargeting, commingling data across multiple advertisers’ campaigns, or allowing piggybacking or redirecting with tags), except on an aggregate and anonymous basis (unless authorized by Facebook) and only to assess the performance and effectiveness of our Facebook advertising campaigns.</p>
+                <p>We do not use Facebook advertising data for any purpose (including retargeting, commingling data across multiple advertisers’ campaigns, or allowing piggybacking or redirecting with tags), except on an aggregate and anonymous basis (unless authorized by Facebook) and only to assess the performance and effectiveness of our Facebook advertising campaigns.</p>
 
-            <p>We do not use Facebook advertising data, including the targeting criteria for a Facebook ad, to build, append to, edit, influence, or augment user profiles, including profiles associated with any mobile device identifier or other unique identifier that identifies any particular user, browser, computer or device.</p>
+                <p>We do not use Facebook advertising data, including the targeting criteria for a Facebook ad, to build, append to, edit, influence, or augment user profiles, including profiles associated with any mobile device identifier or other unique identifier that identifies any particular user, browser, computer or device.</p>
 
-            <p>We do not transfer any Facebook advertising data (including anonymous, aggregate, or derived data) to any ad network, ad exchange, data broker or other advertising or monetization related service.</p>
-            <li>We will protect personal information by using reasonable security safeguards against loss or theft, as well as unauthorized access, disclosure, copying, use or modification.</li>
-            <li>We will only retain personal information for as long as necessary for the fulfilment of those purposes.</li>
-        </ul>
+                <p>We do not transfer any Facebook advertising data (including anonymous, aggregate, or derived data) to any ad network, ad exchange, data broker or other advertising or monetization related service.</p>
+                <li>We will protect personal information by using reasonable security safeguards against loss or theft, as well as unauthorized access, disclosure, copying, use or modification.</li>
+                <li>We will only retain personal information for as long as necessary for the fulfilment of those purposes.</li>
+            </ul>
 
-        <p>Please also note that as our business grows, we may buy or sell various assets. In the unlikely event that we sell some or all of our assets, or one or more of our websites is acquired by another company, information about our users may be among the transferred assets.</p>
+            <p>Please also note that as our business grows, we may buy or sell various assets. In the unlikely event that we sell some or all of our assets, or one or more of our websites is acquired by another company, information about our users may be among the transferred assets.</p>
 
-        <p>We are committed to conducting our business in accordance with these principles in order to ensure that the confidentiality of personal information is protected and maintained.</p>
+            <p>We are committed to conducting our business in accordance with these principles in order to ensure that the confidentiality of personal information is protected and maintained.</p>
 
-        <p>doodDevelopments, LLC may change this privacy policy from time to time at doodDevelopments, LLC's sole discretion.</p>
+            <p>doodDevelopments, LLC may change this privacy policy from time to time at doodDevelopments, LLC's sole discretion.</p>
 
-        <p>If you have any questions or concerns regarding our privacy policy please direct them to: phil(@)traveldeveloper(.)com</p>
+            <p>If you have any questions or concerns regarding our privacy policy please direct them to: phil(@)traveldeveloper(.)com</p>
+        </div>
+        <div id="nanas-recipes" class="">
+            <h2>Nana's Recipes Privacy Policy</h2>
+            <strong>Privacy Policy</strong>
+            <p>doodDevelopments, LLC built the Nana's Recipes app as a Free app. This SERVICE is provided by doodDevelopments, LLC at no cost and is intended for use as is.</p>
+            <p>This page is used to inform visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service.</p>
+            <p>If you choose to use our Service, then you agree to the collection and use of information in relation to this policy. The Personal Information that we collect is used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.</p>
+            <p>The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible at Nana's Recipes unless otherwise defined in this Privacy Policy.</p>
+            <p><strong>Information Collection and Use</strong></p>
+            <p>For a better experience, while using our Service, we may require you to provide us with certain personally identifiable information. The information that we request will be retained by us and used as described in this privacy policy.</p>
+            <div>
+                <p>The app does use third party services that may collect information used to identify you.</p>
+                <p>Link to privacy policy of third party service providers used by the app</p>
+                <ul>
+                    <!----><!---->
+                    <li><a href="https://firebase.google.com/policies/analytics" target="_blank" rel="noopener noreferrer">Google Analytics for Firebase</a></li>
+                    <li><a href="https://firebase.google.com/support/privacy/" target="_blank" rel="noopener noreferrer">Firebase Crashlytics</a></li>
+                    <!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!---->
+                </ul>
+            </div>
+            <p><strong>Log Data</strong></p>
+            <p>We want to inform you that whenever you use our Service, in a case of an error in the app we collect data and information (through third party products) on your phone called Log Data. This Log Data may include information such as your device Internet Protocol (“IP”) address, device name, operating system version, the configuration of the app when utilizing our Service, the time and date of your use of the Service, and other statistics.</p>
+            <p><strong>Cookies</strong></p>
+            <p>Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers. These are sent to your browser from the websites that you visit and are stored on your device's internal memory.</p>
+            <p>This Service does not use these “cookies” explicitly. However, the app may use third party code and libraries that use “cookies” to collect information and improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some portions of this Service.</p>
+            <p><strong>Service Providers</strong></p>
+            <p>We may employ third-party companies and individuals due to the following reasons:</p>
+            <ul>
+                <li>To facilitate our Service;</li>
+                <li>To provide the Service on our behalf;</li>
+                <li>To perform Service-related services; or</li>
+                <li>To assist us in analyzing how our Service is used.</li>
+            </ul>
+            <p>We want to inform users of this Service that these third parties have access to your Personal Information. The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose.</p>
+            <p><strong>Security</strong></p>
+            <p>We value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.</p>
+            <p><strong>Links to Other Sites</strong></p>
+            <p>This Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, we strongly advise you to review the Privacy Policy of these websites. We have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.</p>
+            <p><strong>Children’s Privacy</strong></p>
+            <p>These Services do not address anyone under the age of 13. We do not knowingly collect personally identifiable information from children under 13 years of age. In the case we discover that a child under 13 has provided us with personal information, we immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us so that we will be able to do necessary actions.</p>
+            <p><strong>Changes to This Privacy Policy</strong></p>
+            <p>We may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page.</p>
+            <p>This policy is effective as of 2021-10-14</p>
+            <p><strong>Contact Us</strong></p>
+            <p>If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us at phil@pjmanning.dev.</p>
+        </div>
     </div>
 </template>
 

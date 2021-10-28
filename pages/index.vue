724058868cae160c52bcc6d025bae93d14f45cb4
PJ Manning <phil.manning11@gmail.com>
2021-10-28 15:20:25 +0800

    adding nanasrecipes and frontmatter plugin

--- pages/index.vue
+++ pages/index.vue
@@ -232,6 +232,7 @@
                             </div>
                         </div>
                     </li> -->
+
                     <!-- De-Facebook-Me  -->
                     <!-- <li class="col-span-1 border border-gray-100 rounded-lg shadow bg-background-card">
                         <div class="flex items-center justify-between w-full p-6 space-x-6">
@@ -256,6 +257,7 @@
                             </div>
                         </div>
                     </li> -->
+
                     <!-- STARTSMALL  -->
                     <li class="col-span-1 border border-gray-100 rounded-lg shadow bg-background-card">
                         <div class="flex items-center justify-between w-full p-6 space-x-6">
@@ -275,6 +277,47 @@
                                             <path d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zM3 10a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H4a1 1 0 01-1-1v-6zM14 9a1 1 0 00-1 1v6a1 1 0 001 1h2a1 1 0 001-1v-6a1 1 0 00-1-1h-2z" />
                                         </svg>
                                         <span class="ml-3">Website</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+
+                    <!-- Nana's Recipes -->
+                    <li class="col-span-1 border border-gray-100 rounded-lg shadow bg-background-card">
+                        <div class="flex items-center justify-between w-full p-6 space-x-6">
+                            <div class="flex-1 truncate">
+                                <div class="flex items-center space-x-3">
+                                    <h3 class="text-xl font-medium leading-5 text-gray-900 truncate">Nana's Recipes</h3>
+                                </div>
+                                <p class="mt-1 text-sm leading-5 text-gray-500 truncate">Nana Scoppetuolo's best recipes.</p>
+                            </div>
+                            <img class="flex-shrink-0 w-12 h-12 bg-gray-300 rounded-md" src="~/assets/images/home/nanasrecipes-logo.png" alt="nanas recipes" />
+                        </div>
+                        <div class="bg-gray-100 border-t border-gray-200 rounded-b-lg">
+                            <div class="flex -mt-px">
+                                <!-- <div class="flex flex-1 w-0 border-r border-gray-200">
+                                    <a href="https://swellpirate.com" target="_blank" class="relative inline-flex items-center justify-center flex-1 w-0 py-4 -mr-px text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out border border-transparent rounded-bl-lg hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:z-10">
+                                        <svg class="w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
+                                            <path d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zM3 10a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H4a1 1 0 01-1-1v-6zM14 9a1 1 0 00-1 1v6a1 1 0 001 1h2a1 1 0 001-1v-6a1 1 0 00-1-1h-2z" />
+                                        </svg>
+                                        <span class="ml-3">Website</span>
+                                    </a>
+                                </div> -->
+                                <div class="flex flex-1 w-0 -ml-px border-r border-gray-200">
+                                    <a href="https://apps.apple.com/app/nana-scoppetuolos-recipes/id1012100193?l=id" target="_blank" class="relative inline-flex items-center justify-center flex-1 w-0 py-4 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out border border-transparent rounded-br-lg hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:z-10">
+                                        <svg class="w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                        </svg>
+                                        <span class="ml-3">iOS</span>
+                                    </a>
+                                </div>
+                                <div class="flex flex-1 w-0 -ml-px">
+                                    <a href="https://play.google.com/store/apps/details?id=com.dooddevelopments.Recipe" target="_blank" class="relative inline-flex items-center justify-center flex-1 w-0 py-4 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out border border-transparent rounded-br-lg hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10">
+                                        <svg class="w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                        </svg>
+                                        <span class="ml-3">Android</span>
                                     </a>
                                 </div>
                             </div>

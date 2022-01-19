e98dcb978c61608d39bdcfc01b6765b428bd2fe5
PJ Manning <phil.manning11@gmail.com>
2022-01-19 14:30:18 +0800

    adding privacy page

---
+++ pages/privacy.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="max-w-6xl mx-auto my-20">
+        <h1 class="mb-8 text-4xl font-bold">Privacy Policies</h1>
+
+        <div>
+            <h2 class="mb-4 text-2xl font-semibold text-left">Nana Scoppetuolo's Recipe App</h2>
+            <p>We don't collect anything from you. We create an anonymous account in our backend (Google Firebase) which we use to store your favorite recipes. This is tied to your device and we have no way to know it's you.</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
